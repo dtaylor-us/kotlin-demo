01a9fb4673d10088a21195115ee489de01cb9a88
dtaylor-us <derektaylor.us@gmail.com>
2020-12-22 15:48:00 -0600

    using npm packages

--- src/main/kotlin/ReactPlayer.kt
+++ src/main/kotlin/ReactPlayer.kt
@@ -2,6 +2,11 @@
 @file:JsNonModule
 
 import react.RClass
+import react.RProps
 
 @JsName("default")
-external val reactPlayer: RClass<dynamic>
+external val reactPlayer: RClass<ReactPlayerProps>
+
+external interface ReactPlayerProps : RProps {
+    var url: String
+}
